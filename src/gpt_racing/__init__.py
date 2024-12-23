72e609d9ab7bcf1cb4850410479267eddc961ef6
Logan Grado <grado.logan@gmail.com>
2024-12-23 16:40:09 -0500

    Add loggerado

--- src/gpt_racing/__init__.py
+++ src/gpt_racing/__init__.py
@@ -1,9 +1,11 @@
 import logging
 from pathlib import Path
+import loggerado
 
 __version__ = "0.1.0"
 
 logger = logging.getLogger(__name__)
+loggerado.configure_logger(logger, "INFO", ansi=True)
 
 ROOT = Path(__file__).parent
 SECRETS_PATH = ROOT.parent.parent / "secrets.json"
